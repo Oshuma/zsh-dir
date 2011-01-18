c880186ce64cf259aead8961b2a7aa3ddd516db6
Dale Campbell <oshuma@gmail.com>
2011-01-17 22:22:30 -0500

    this is better

--- lib/nethack.zsh
+++ lib/nethack.zsh
@@ -9,13 +9,21 @@ alias cnethack=$local_nethack
 # Uses ttyrec to record nethack sessions.
 function rnethack() {
   recordings_dir="$HOME/.nethack/recordings"
+  recording_path="$recordings_dir/`date +%Y`/`date +%m`"
+  recording_name="`date +%d-%H%M%S`.ttyrec"
+  recording_file="$recording_path/$recording_name"
 
   if [[ ! -w $recordings_dir ]]; then
     echo "Nethack recordings directory '$recordings_dir' not found or not writable."
     return
   fi
 
-  recording_path="$recordings_dir/nethack-`date +%Y.%m.%d-%H%M%S`.ttyrec"
-  ttyrec -e "nethack $*" $recording_path
-  bzip2 $recording_path
+  if [[ ! -w $recording_path ]]; then
+    echo "Creating: $recording_path"
+    mkdir -p $recording_path
+  fi
+
+  echo "Recording Nethack session to: $recording_file"
+  ttyrec -e "nethack $*" $recording_file
+  bzip2 $recording_file
 }
